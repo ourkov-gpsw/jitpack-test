2b92dfc5d49fb857082b3fa20bbac15abb3a9310
Owen Urkov <ourkov@gopro.com>
2016-07-05 12:40:20 -0700

    initial commit

---
+++ src/main/java/DateUtils.java
@@ -0,0 +1,16 @@
+
+package com.ourkov;
+
+import java.util.Date;
+
+public class DateUtils{
+	
+	public static void main(String[] args) {
+		System.out.println(getCurrentDate());
+	}
+	
+	private static Date getCurrentDate(){
+		return new Date();
+	}
+	
+}
